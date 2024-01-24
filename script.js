341da3b724839fb14cc74344b68317dd742fc373
Manstzy <muh.arhief20@gmail.com>
2024-01-24 21:04:06 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// alert("Hello Java script anda telah berhasil di connectkan")
+// alert("Saya datang")
+// console.log("Saya Ngoding");
+
+// function penjumlahan (num1 , num2) {
+//     return num1 + num2
+// }
+
+// var lokasi = "blitar"
+// lokasi = "jogja"
+// console.log(location)
+
+let harga = 2000
+harga = 500
+console.log(harga)
+let nama = "arif"
+let hargaPangan = 2.9
+let isTrue = true // false
+
